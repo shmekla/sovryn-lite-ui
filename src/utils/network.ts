438858d78624002347b94cf5b7d26f52162a8050
harmaus <admin@defraylabs.com>
2021-08-29 22:26:37 +0300

    fix: linter

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -2,7 +2,7 @@ import type { NETWORK, NetworkSettings } from 'types/network';
 import networks from '../config/networks';
 
 export function getCurrentNetwork() {
-  return Object.values(networks).find(item => item.id === Number(process.env.NETWORK_ID || '31')) as NetworkSettings;
+  return Object.values(networks).find(item => item.id === Number(process.env.REACT_APP_NETWORK_ID || '31')) as NetworkSettings;
 }
 
 export function getNetwork(network: NETWORK) {
