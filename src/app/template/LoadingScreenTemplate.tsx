f820ad7afda99dd16c118417644fe6b027e8c893
harmaus <admin@defraylabs.com>
2021-09-27 10:24:03 +0300

    internal: changed logo on index and loading screen.

--- src/app/template/LoadingScreenTemplate.tsx
+++ src/app/template/LoadingScreenTemplate.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { ReactComponent as Logo } from 'assets/bitcoin-btc-logo.svg';
+import { ReactComponent as Logo } from 'assets/logo.svg';
 
 const LoadingScreenTemplate: FC = () => <div className="w-screen h-screen flex flex-col justify-center items-center">
   <Logo className="w-24 h-24 mb-4"/>
