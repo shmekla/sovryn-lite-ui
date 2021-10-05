b93865910e682e67ddd457e07d2c5d3eb090f0b3
harmaus <admin@defraylabs.com>
2021-10-05 13:31:43 +0300

    hotfix: discord invite link

--- src/config/socialLinks.ts
+++ src/config/socialLinks.ts
@@ -2,7 +2,7 @@ const socialLinks = {
   email: 'hello@defraylabs.com',
   // telegramInvite: 'https://t.me/joinchat/zm9bhjrchdo1MTA0',
   // invites to sovryn-lite channel of defray community server
-  discordInvite: 'https://discord.gg/AyqPuKZHrJ',
+  discordInvite: 'https://discord.gg/TdbWUhU',
   githubRepository: 'https://github.com/defray-labs/sovryn-lite-ui',
   twitterUrl: 'https://twitter.com/DefrayLabs',
 };
